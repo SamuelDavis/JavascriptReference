4582d05ec8c87b7cc6903a1e6418c35079396905
Samuel Davis <sam@bluehousegroup.com>
2013-07-28 09:50:25 -0400

    Updated jQuery include to reference jquery.com's latest release.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
 </head>
 <body>
 	<div id="output"></div>
